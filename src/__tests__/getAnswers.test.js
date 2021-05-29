4840df2905fea9de1e6686adb105a6bf25d7df54
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-29 20:41:07 +0100

    added additional test in getAnswers.test.js

--- src/__tests__/getAnswers.test.js
+++ src/__tests__/getAnswers.test.js
@@ -7,22 +7,34 @@ jest.mock("inquirer");
 describe("getAnswers", () => {
   const inquirerSpy = jest.spyOn(inquirer, "prompt");
 
+  const mockQuestions = [
+    {
+      message: "mock message",
+      name: "mock",
+    },
+  ];
+
   const mockAnswers = (answers = {}) => {
     inquirerSpy.mockResolvedValue(answers);
   };
 
+  beforeAll(() => {
+    jest.clearAllMocks();
+  });
+
   test("should pass questions to inquirer prompt", async () => {
     mockAnswers();
-
-    const mockQuestions = [
-      {
-        message: "mock message",
-        name: "mock",
-      },
-    ];
 
     await getAnswers(mockQuestions);
 
     expect(inquirerSpy).toHaveBeenCalledWith(mockQuestions);
+  });
+
+  test("should return the expected answers", async () => {
+    mockAnswers({ mock: "foo bar" });
+
+    const actual = await getAnswers(mockQuestions);
+
+    expect(actual).toEqual({ mock: "foo bar" });
   });
 });
